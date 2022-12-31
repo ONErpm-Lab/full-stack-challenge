36599dad20f6d35c29be161b83ed343227bb12b6
letix <leticia.carvalho@athonenergia.com.br>
2022-12-31 14:51:28 -0300

    feat: converts debutDate and Milliseconds at SongMapper

--- src/domain/mappers/SongMapper.ts
+++ src/domain/mappers/SongMapper.ts
@@ -1,16 +1,15 @@
 import { Song } from "../models/Song";
-
+import DateTime from "../../shared/converters/DateTime"
 export class SongMapper {
-
     static toResponse(song: any): Song {
         const songCreated = Song.create({
             "ISRC": song.ISRC,
             "name": song.name,
             "thumbFile": song.thumbFile,
             "thumbFileIcon": song.thumbFileIcon,
-            "debutDate": song.debutDate,
+            "debutDate": DateTime.dateTimeToDate(song.debutDate),
             "artists": song.artists,
-            "miliseconds": song.miliseconds,
+            "miliseconds": DateTime.millisToMinutesAndSeconds(song.miliseconds),
             "previewFile": song.previewFile,
             "spotifyUrl": song.spotifyUrl,
             "isAvaibleAtCountry": song.isAvaibleAtCountry
