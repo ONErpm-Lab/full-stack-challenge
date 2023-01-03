415ee64bd67a4a67987a73b17c968d69814c9bb0
letix <leticia.carvalho@athonenergia.com.br>
2023-01-03 13:34:16 -0300

    fix: response can have only message or only data

--- src/shared/core/Responses.ts
+++ src/shared/core/Responses.ts
@@ -1,7 +1,7 @@
 import Errors from "./ErrorHandler"
 
 export default class Responses extends Errors {
-    static success (message: string, data?: any) {
-        return { code: 200, message: message, data: data ? data : '' }
+    static success (response: { message?: string, data?: any }) {
+        return { code: 200, message: response.message, data: response.data }
     }
 }
