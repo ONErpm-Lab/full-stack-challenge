7d98a69820a6786366fa0823181be1e2911156b8
letix <leticia.carvalho@athonenergia.com.br>
2023-01-03 01:33:52 -0300

    refactor: Spotify class uses Variables and SongMapper toFormat function

--- src/adapters/Spotify.ts
+++ src/adapters/Spotify.ts
@@ -1,11 +1,13 @@
+import { SongMapper } from './../domain/mappers/SongMapper'
 import { Song } from './../domain/models/Song'
+import Variables from '../shared/configs/Variables'
 import fetch from 'node-fetch'
 
 export class Spotify {
 
     async constructSongListByISRC (ISRC: string): Promise<Song> {
         try{
-            const spotifySearchAPI: string = `https://api.spotify.com/v1/search`
+            const spotifySearchAPI: string = `${Variables.SPOTIFY_PUBLIC_API}/v1/search`
 
             const access_token = await this.getAccessToken()
 
@@ -17,7 +19,7 @@ export class Spotify {
 
             const data = await response.json()
 
-            const songFormatted = await this.formatSongFromSpotify(data.tracks.items[0])
+            const songFormatted = await SongMapper.toFormat(data.tracks.items[0])
 
             const song = Song.create(songFormatted)
 
@@ -29,12 +31,12 @@ export class Spotify {
 
     private async getAccessToken() {
         try {
-            const response = await fetch('https://accounts.spotify.com/api/token', {
+            const response = await fetch(`${Variables.SPOTIFY_ACCOUNTS_API}/api/token`, {
                 method: 'POST',
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded"
                 },
-                body: process.env['SPOTIFY_CREDENTIALS']
+                body: Variables.SPOTIFY_CREDENTIALS
             })
 
             const data = await response.json()
@@ -46,45 +48,4 @@ export class Spotify {
         }
     }
 
-    private async formatSongFromSpotify (data: any): Promise<any> {
-        return {
-            "ISRC": data.external_ids.isrc,
-            "name": data.name,
-            "thumbFile": data.album.images[0].url,
-            "thumbFileIcon": data.album.images[2].url,
-            "debutDate": data.album.release_date,
-            "artists": await this.getArtists(data.artists),
-            "milliseconds": data.duration_ms,
-            "previewFile": data.preview_url,
-            "spotifyUrl": data.external_urls.spotify,
-            "isAvaibleAtCountry": await this.isAvaibleAtCountry(data.available_markets),
-        }
-
-    }
-
-    private async getArtists (artists: Array<{name: string, external_urls: {spotify: string}}>) {
-
-        const artistsList: Array<{}> = []
-
-        artists.forEach(artist => {
-            artistsList.push(artist.name)
-        })
-
-        const artistsListString: Array<String> = []
-
-        artistsList.forEach((artistJson) => {
-            artistsListString.push(String(artistJson))
-        })
-
-        return artistsListString.toString()
-    }
-
-    private async isAvaibleAtCountry (countries: Array<string>) {
-
-        if(countries.includes('BR')) {
-            return true
-        } else {
-            return false
-        }
-    }
 }
