7a8e67e91c82a2c80dea4cce031e409a724f724b
letix <leticia.carvalho@athonenergia.com.br>
2023-01-03 01:00:28 -0300

    feat: using cors at API response

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,10 +1,19 @@
 import express from 'express'
 import song from './songs'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
+
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: false,
+}))
 
 app.use('/songs', song)
 
