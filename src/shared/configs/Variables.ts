ef92291ad0e24b1cb6e2c87cd3b783403ed0a825
letix <leticia.carvalho@athonenergia.com.br>
2023-01-03 01:33:00 -0300

    feat: starts Variables, a object that keeps the project env and others shared variables

---
+++ src/shared/configs/Variables.ts
@@ -0,0 +1,11 @@
+export default {
+    SPOTIFY_CREDENTIALS: process.env['SPOTIFY_CREDENTIALS'],
+    DB_HOST: process.env['DB_HOST'],
+    DB_USER: process.env['DB_USER'],
+    DB_PASS: process.env['DB_PASS'],
+    DB_PORT: process.env['DB_PORT'],
+    DB_SCHEMA: process.env['DB_SCHEMA'],
+    PORT: process.env['PORT'],
+    SPOTIFY_PUBLIC_API: 'https://api.spotify.com',
+    SPOTIFY_ACCOUNTS_API: 'https://accounts.spotify.com'
+}
