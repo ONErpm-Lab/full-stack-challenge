b78a8b146465bc117da693250902d84326ededbc
letix <leticia.carvalho@athonenergia.com.br>
2023-01-03 01:31:39 -0300

    feat: add toFormat at SongMapper, that recieves a spotify API data

--- src/domain/mappers/SongMapper.ts
+++ src/domain/mappers/SongMapper.ts
@@ -19,4 +19,47 @@ export class SongMapper {
 
         return songCreated
     }
+
+    static async toFormat(data: any): Promise<any> {
+
+        return {
+            "ISRC": data.external_ids.isrc,
+            "name": data.name,
+            "thumbFile": data.album.images[0].url,
+            "thumbFileIcon": data.album.images[2].url,
+            "debutDate": data.album.release_date,
+            "artists": await this.getArtists(data.artists),
+            "milliseconds": data.duration_ms,
+            "previewFile": data.preview_url,
+            "spotifyUrl": data.external_urls.spotify,
+            "isAvaibleAtCountry": await this.isAvaibleAtCountry(data.available_markets),
+        }
+
+    }
+
+    private static async getArtists (artists: Array<{name: string, external_urls: {spotify: string}}>) {
+
+        const artistsList: Array<{}> = []
+
+        artists.forEach(artist => {
+            artistsList.push(artist.name)
+        })
+
+        const artistsListString: Array<String> = []
+
+        artistsList.forEach((artistJson) => {
+            artistsListString.push(String(artistJson))
+        })
+
+        return artistsListString.toString()
+    }
+
+    private static async isAvaibleAtCountry (countries: Array<string>) {
+
+        if(countries.includes('BR')) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
