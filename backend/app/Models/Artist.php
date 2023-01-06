61319fe79850770d260ac6a7f255afce4baaa323
cassiofb-dev <cassiofb.souza@gmail.com>
2023-01-06 17:21:53 -0300

    feat: use soft delete

--- backend/app/Models/Artist.php
+++ backend/app/Models/Artist.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Artist extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'artists';
 
     protected $fillable = ['spotify_id', 'name'];
--- backend/app/Models/Track.php
+++ backend/app/Models/Track.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Track extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'tracks';
 
-    protected $fillable = ['isrc', 'thumb_url', 'release_date', 'title', 'length'];
+    protected $fillable = ['isrc', 'thumb_url', 'release_date', 'title', 'length', 'spotify_url', 'br_avaiable'];
 
     public function set($data)
     {
