9698c2365d89281f4be7449c2ac85ce2b835acc6
cassiofb-dev <cassio@onerpm.com>
2023-02-08 12:29:04 -0300

    chore: change database docker services

--- backend/config/database.php
+++ backend/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'main'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,23 +35,15 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql' => [
+        'main' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'url' => env('MAIN_DB_URL'),
+            'host' => env('MAIN_DB_HOST', '127.0.0.1'),
+            'port' => env('MAIN_DB_PORT', '3306'),
+            'database' => env('MAIN_DB_DATABASE', 'forge'),
+            'username' => env('MAIN_DB_USERNAME', 'forge'),
+            'password' => env('MAIN_DB_PASSWORD', ''),
+            'unix_socket' => env('MAIN_DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -61,6 +53,34 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+        ],
+
+        'secondary' => [
+            'driver' => 'mysql',
+            'url' => env('SECONDARY_DB_URL'),
+            'host' => env('SECONDARY_DB_HOST', '127.0.0.1'),
+            'port' => env('SECONDARY_DB_PORT', '3306'),
+            'database' => env('SECONDARY_DB_DATABASE', 'forge'),
+            'username' => env('SECONDARY_DB_USERNAME', 'forge'),
+            'password' => env('SECONDARY_DB_PASSWORD', ''),
+            'unix_socket' => env('SECONDARY_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'pgsql' => [
